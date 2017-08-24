a65cc03e44e2f447f2cd433d26e7c5023265471d
KwinstonRoberts <kyl.w.r@hotmail.com>
2017-08-24 01:12:57 +0000

    reduce undry code in message.jsx

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,25 +1,13 @@
-
-
-import React, { Component } from 'react';
-
+import React from 'react';
 import Message from './Message.jsx';
 
-class MessageList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      messages: this.props.messages,
-    };
-  }
-  render() {
-    return (
-      <main className="messages">
-        <Message messages={this.props.messages}/>
-        <div className="message system">{this.props.notification}</div>
-      </main>
-      );
-  }
-}
+let MessageList = (props) =>
+
+  <main className="messages">
+    <Message messages={props.messages}/>
+    <div className="message system">{props.notification}</div>
+  </main>
+
 MessageList.propTypes = {
   messages: React.PropTypes.array,
   color:React.PropTypes.string,
